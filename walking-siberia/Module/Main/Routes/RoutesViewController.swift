0f02879a6562d6ed0a2d2a22ce7b7acc4312bd2a
zloysergunya <zloysergunya@mail.ru>
2022-05-01 20:04:59 +0700

    * | Routes | Fix user activity sending

--- walking-siberia/Module/Main/Routes/RoutesViewController.swift
+++ walking-siberia/Module/Main/Routes/RoutesViewController.swift
@@ -32,7 +32,10 @@ class RoutesViewController: ViewController<RoutesView> {
         mainView.stepsCountView.setup(with: 0, distance: 0.0)
         
         syncContacts()
-        syncUserActivity()
+        
+        DispatchQueue.main.async { [weak self] in
+            self?.syncUserActivity()
+        }
     }
     
     override func viewWillAppear(_ animated: Bool) {
