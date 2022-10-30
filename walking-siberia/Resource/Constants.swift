d55a1606e877c12dee516263aa61c22d45ae2d1d
zloysergunya <zloysergunya@mail.ru>
2022-10-30 13:45:39 +0700

    + | TabBar | Add switch to dev/prod

--- walking-siberia/Resource/Constants.swift
+++ walking-siberia/Resource/Constants.swift
@@ -3,7 +3,12 @@ import UIKit
 
 enum Constants {
 
+    // MARK: - API
+    static let apiVersion = 1
     static let pageLimit = 20
+    static var prodUrl: String { "https://api.walking-siberia.ru/v\(apiVersion)" }
+    static var devUrl: String { "http://185.20.225.129:8005/v\(apiVersion)" }
+    static var basePath: String { UserSettings.isDev ? devUrl: prodUrl }
     
     static let appStoreId = ""
     static let bundleIdentifier = Bundle.current.bundleIdentifier ?? "ru.abs.walking-siberia"
